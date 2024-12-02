3d37ef253b029a0343fe2192f27a1751ce565f55
awedi ahmed <awediahmed75@gmail.com>
2024-12-02 20:58:43 +0100

    :sparkles: results extraction

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ from algorithms.bga_algorithm import run_genetic_algorithm_executions
 
 C1 = [0.5, 0.4]  # bga1 c1=0.5 c2 = 0.4 bga2 c1=1.5 c2=1.4
 C2 = [1.5, 1.5]
-execution_type = 2
+execution_type = 1
 
 
 if __name__ == "__main__":
     if execution_type == 1:
         run_pso_algorithm_executions(C1, "src/results/bpso1-results")
-        run_genetic_algorithm_executions(0.05, "src/results/bga1-results")
+        run_genetic_algorithm_executions(0.5, "src/results/bga1-results")
     else:
-        # run_pso_algorithm_executions(C2, "src/results/bpso2-results")
+        run_pso_algorithm_executions(C2, "src/results/bpso2-results")
         run_genetic_algorithm_executions(0.05, "src/results/bga2-results")
 #
